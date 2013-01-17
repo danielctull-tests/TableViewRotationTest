b01a4cfa8926d027fd43f34f3e4ffecea35e7d29
Daniel Tull <dt@danieltull.co.uk>
2013-01-17 19:16:18 +0000

    Initial Commit

---
+++ TableViewRotationTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableViewRotationTest
+//
+//  Created by Daniel Tull on 17.01.2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
